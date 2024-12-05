c343fac0cf132b9063e0036ffbe8c5f93b41e882
hm faisal <contact.hmfaisal@gmail.com>
2024-12-05 22:01:00 +0600

    sort functionality implemeted

--- src/pages/home/Product.jsx
+++ src/pages/home/Product.jsx
@@ -3,20 +3,39 @@ import ProductCard from "./ProductCard";
 
 const Product = () => {
   const [products, setProducts] = useState([]);
+  const [sortProducts, setSortProducts] = useState(false);
   useEffect(() => {
     fetch("http://localhost:5000/all_equipment")
       .then((res) => res.json())
       .then((data) => setProducts(data));
   }, []);
+
+  const sortHandler = () => {
+    setProducts(products.sort((a, b) => a.eqPrice - b.eqPrice));
+    setSortProducts(true);
+    console.log(products);
+  };
   return (
     <>
       <div className="flex justify-center items-center flex-col my-16">
-        <h2 className="text-4xl font-bold">See Our Product</h2>
+        <div className="flex mb-6 justify-around w-full">
+          <h2 className="text-4xl font-bold">See Our Product</h2>{" "}
+          <button className="btn" onClick={sortHandler}>
+            Sort By Price
+          </button>
+        </div>
+
         <div className="Products grid grid-cols-3 gap-4">
           {products.length > 0 ? (
-            products.map((product, i) => (
-              <ProductCard product={product} key={i} />
-            ))
+            sortProducts ? (
+              products.map((product, i) => (
+                <ProductCard product={product} key={i} />
+              ))
+            ) : (
+              products.map((product, i) => (
+                <ProductCard product={product} key={i} />
+              ))
+            )
           ) : (
             <div>No Product found</div>
           )}
